342aa0d400d6c8f8b69b0d7e1c4b427263738bcb
Dobiecodes <Dobiecodes@gmail.com>
2023-08-04 09:57:31 -0700

    switched out localhost:1337 for https://dobie-fullstackrestaurantapplication.onrender.com/

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,8 +5,7 @@ const nextConfig = {
       remotePatterns: [
         {
           protocol: "http",
-          hostname: "127.0.0.1",
-          port: "1337",
+          hostname: "dobie-fullstackrestaurantapplication.onrender.com/",
           pathname: "/uploads/**",
         },
       ],
